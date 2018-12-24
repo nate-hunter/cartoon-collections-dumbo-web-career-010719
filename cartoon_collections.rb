45884cd2468ee5e2f3d40055420fcc7c9cc6d40c
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-24 05:44:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,6 @@ end
 
 
 def find_the_cheese (array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find { |cheese|
     cheese_types.include?(cheese)
