41ce373634de6882d6ca19f6ca90eb4170000ccd
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-24 05:44:55 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (array)
+  array.each_with_index { | name, order |
+    puts "#{[order+1]}. #{name}" 
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# capitalize each element and add an exclamation point at the end.
+def summon_captain_planet (array)
+  array.map { |element|
+    element.capitalize + "!"
+  }
+end 
+
+# The method should tell us if any of the calls are longer than four characters.
+def long_planeteer_calls (array)
+  array.any? { |calls|
+    calls.length > 4
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |cheese|
+    cheese_types.include?(cheese)
+  }
 end
